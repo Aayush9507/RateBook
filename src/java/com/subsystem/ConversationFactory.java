d094d4399db2ba651ae61e406db187dc36496829
aayush goyal <aayushgoyal9507@gmail.com>
2019-04-22 18:51:34 -0600

    Added Envelope factory

--- src/java/com/subsystem/ConversationFactory.java
+++ src/java/com/subsystem/ConversationFactory.java
@@ -1,8 +1,15 @@
 package com.subsystem;
 
+import com.message.ACKMessage;
+import com.message.CreateProdMessage;
+import com.message.FollowProdMessage;
+import com.message.LoginUserMessage;
 import com.message.Message;
 import com.message.Message.MessageType;
+import com.message.RateFeedMessage;
+import com.message.RateProdMessage;
 import com.message.RegisterUserMessage;
+import com.message.SearchProductMessage;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
@@ -17,15 +24,69 @@ public class ConversationFactory {
         return conv;
     }
 
-    public Envelope CreateEnvelopee(String convId,String userId, String Name, String emailId, String password, String areaOfInterest) throws IOException {
+    public Envelope CreateEnvelopee(ArrayList<String> msglist) throws IOException {
+        
+        Message msg;
         System.out.println("Creating Envelope using factory");
-        RegisterUserMessage rum;
-        String msgId = UUID.randomUUID().toString();
-        rum = new RegisterUserMessage(userId, Name, emailId, password, areaOfInterest);
-        System.out.println("rummmmm"+rum.getAreaOfInterest()+rum.getName()+rum.getEmailId()+rum.getUserId()+rum.getPassword());
-        Envelope env = new Envelope(rum, new InetSocketAddress("localhost", 8082));
+        EnvelopeFactory ef = new EnvelopeFactory();
+        msg = ef.EnvelopeFactory(msglist);
+        
+//        String msgId = UUID.randomUUID().toString();
+//        msg = new RegisterUserMessage(userId, Name, emailId, password, areaOfInterest);
+//        System.out.println("rummmmm"+rum.getAreaOfInterest()+rum.getName()+rum.getEmailId()+rum.getUserId()+rum.getPassword());
+        Envelope env = new Envelope(msg, new InetSocketAddress("localhost", 8082));
 //        byte [] b = rum.encode();
 //        System.out.println("No. of bytes after creating and encoding the object"+b.length);
         return env;
     }
+    
+public class EnvelopeFactory {
+    
+    
+    public UUID uuid = UUID.randomUUID();
+    
+    public Message EnvelopeFactory(ArrayList<String> mylist){
+            
+            String pos = mylist.get(0);
+            	
+            
+            if (pos.equals("RegisterUser"))
+            {
+                return new RegisterUserMessage(uuid, mylist.get(1), mylist.get(2), mylist.get(3), mylist.get(4), mylist.get(5));
+            }
+            
+            if (pos.equals("LoginUser"))
+            {
+                return new LoginUserMessage(uuid,mylist.get(1), mylist.get(2));
+            }
+            
+            if (pos.equals("CreateProd"))
+            {
+                return new CreateProdMessage(uuid, mylist.get(1), mylist.get(2), mylist.get(3), Short.valueOf(mylist.get(4)));
+            }
+            
+            if (pos.equals("FollowProd"))
+            {
+                return new FollowProdMessage(uuid, mylist.get(1), mylist.get(2));
+            }
+            if (pos.equals("RateProd"))
+            {
+                return new RateProdMessage(uuid, mylist.get(1), Float.valueOf(mylist.get(2)),mylist.get(3));
+            }
+            if (pos.equals("RateFeed"))
+            {
+                return new RateFeedMessage(uuid, mylist.get(1), Short.valueOf(mylist.get(2)),mylist.get(3),mylist.get(4));
+            }
+            if (pos.equals("SearchProd"))
+            {
+                return new SearchProductMessage(uuid, mylist.get(1), mylist.get(2));
+            }
+            if (pos.equals("ACKMessage"))
+            {
+                return new ACKMessage(uuid);
+            }
+            return null;
+    }
 }
+}
+
